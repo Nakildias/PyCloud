f49a720441cc6cb9f8f8c92c96c0f102344df56b
Nakildias <130524745+Nakildias@users.noreply.github.com>
2025-05-21 00:31:43 -0400

    Update install_or_update.sh
    Added yt-dlp to pip deps

--- install_or_update.sh
+++ install_or_update.sh
@@ -36,6 +36,7 @@ PYTHON_DEPS=( # Python packages to install via pip
     "markdown"
     "humanize"
     "gitpython"
+    "yt-dlp"
 )
 MAIN_EXECUTABLE_NAME="PyCloud" # Name of the script to link in TARGET_BIN_DIR
 LINK_NAMES=( "pycloud" ) # Additional names (symlinks)
