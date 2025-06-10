879874b6c708ed5aaf9020d93296248b4f856315
Nakildias <130524745+Nakildias@users.noreply.github.com>
2025-06-10 02:17:58 -0400

    Update models.py
    Increase password hash max lenght

--- app/models.py
+++ app/models.py
@@ -54,7 +54,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(1024), nullable=False)
     is_admin = db.Column(db.Boolean, default=False, nullable=False)
     storage_limit_mb = db.Column(db.Integer, nullable=True)
     bio = db.Column(db.String(2500), nullable=True)
