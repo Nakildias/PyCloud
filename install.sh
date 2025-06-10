911c883600624c936faea2a94f5966cc6fa5e078
Nakildias <130524745+Nakildias@users.noreply.github.com>
2025-06-09 21:33:11 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,6 +37,7 @@ PYTHON_DEPS=( # Python packages to install via pip
     "yt-dlp"
     "Flask-SocketIO"
     "paramiko"
+    "eventlet"
     "py7zr"
 )
 MAIN_EXECUTABLE_NAME="PyCloud" # Name of the script to link in TARGET_BIN_DIR
